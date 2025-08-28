d5c9435475108bcdd75446380b10e4170de45613
Pamela Argentino <argentinopam@hotmail.com>
2025-08-28 13:51:57 -0300

    add amplify and tailwindcss

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,11 @@
+import { Amplify } from 'aws-amplify';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import outputs from '../amplify_outputs.json';
 import App from './App.tsx';
 import './index.css';
+
+Amplify.configure(outputs);
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
